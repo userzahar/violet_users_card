1d894bcffe8dd7e0ee188c16c714c9e40994c33f
userzahar <110730909+userzahar@users.noreply.github.com>
2023-05-07 01:50:16 +0300

    fix route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,9 @@ import { Route, Routes } from "react-router-dom"
 import { Suspense } from "react";
 import { lazy } from "react";
 import { Header } from "./Header/Header";
+import Homepage from "pages/Homepage/Homepage";
 
-const Homepage = lazy(() => import('pages/Homepage/Homepage'));
+// const Homepage = lazy(() => import('pages/Homepage/Homepage'));
 const Tweets = lazy(() => import('pages/Tweets/Tweets'));
 
 export const App = () => {
@@ -13,7 +14,7 @@ export const App = () => {
       <Routes>
           <Route path="/" element={<Header/>}>
                 <Route index element={<Homepage />} />
-                <Route path="*" element={<Homepage />} />
+                {/* <Route path="*" element={<Homepage />} /> */}
                 <Route path="/tweets" element={<Tweets />} />
           </Route>
       </Routes>   
