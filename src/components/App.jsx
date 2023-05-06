41c6e608ece3691ecc4f1f5f0cfb0ec18a6d47e3
userzahar <110730909+userzahar@users.noreply.github.com>
2023-05-07 02:08:43 +0300

    fix it

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,13 @@
 import { Route, Routes } from "react-router-dom"
-import { Suspense } from "react";
 import { lazy } from "react";
-import { Header } from "./Header/Header";
 import Homepage from "pages/Homepage/Homepage";
 
-// const Homepage = lazy(() => import('pages/Homepage/Homepage'));
+const Homepage = lazy(() => import('pages/Homepage/Homepage'));
 const Tweets = lazy(() => import('pages/Tweets/Tweets'));
 
 export const App = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+
       <Routes>
           <Route path="/" element={<Header/>}>
                 <Route index element={<Homepage />} />
@@ -17,7 +15,7 @@ export const App = () => {
                 <Route path="/tweets" element={<Tweets />} />
           </Route>
       </Routes>   
-    </Suspense>
+
   );
 };
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,18 @@
 
+import { Suspense } from "react"
 import { HeaderLink, HeaderStyled } from "./Header.styled"
+import { Outlet } from "react-router-dom"
 
 export const Header = () => {
-    return  <HeaderStyled>
+  return<>
+  <HeaderStyled>
       <nav>
         <HeaderLink to="/">Home</HeaderLink>
         <HeaderLink to="/tweets">Tweets</HeaderLink>
       </nav>
-      </HeaderStyled>
+    </HeaderStyled>
+    <Suspense fallback={<div>Loading...</div>}>
+        <Outlet />
+      </Suspense>
+    </>
 }
